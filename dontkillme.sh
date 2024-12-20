15d82578bdae6d789c555c7b1f9f2503322be4bc
mystic76 <contact.iamlynx@gmail.com>
2024-12-20 21:01:55 +0530

    Create dontkillme.sh

---
+++ dontkillme.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/sh
+echo "keep Collab alive"
+echo "made by mystic__76"
+sleep 1
+while true 
+do 
+  # 
+  echo "collab/ruai.ipynb/604.84:2063.79" 
+  sleep 1 
+done 
